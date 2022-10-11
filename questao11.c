5412f7c10f3f4dfbb268f95a0209545a029613a1
HILIZANGELA TALLIA <105807771+Tallia-Sousa@users.noreply.github.com>
2022-10-11 20:21:11 -0300

    Add files via upload

---
+++ questao11.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int negativos(int n, float*vet){
+  int numero = 0;
+  int i = 0;
+  while(i < n){
+    i++;
+    if(vet[i]<0){
+      numero = numero + 1;
+    }
+  }
+  return numero;
+}
+int main(){
+  int j;
+  float vetor[3];
+  printf("Digite os numeros \n");
+  for(j = 0; j < 3; j++){
+    scanf("%f",&vetor[j]);
+  }
+    while(j > 3){
+    j++;
+    printf("%2.f\n", vetor[j]);
+    }
+  printf("\n %d  número(s) negativo (s) ", negativos(3, vetor));
+  return 0;
+}
